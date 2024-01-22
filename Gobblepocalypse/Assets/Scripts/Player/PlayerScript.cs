20c26e19fcb9d3a28b1356b1b3295cc51a41441b
Tikotine <timothykok@live.com>
2024-01-22 15:04:45 +0800

    Attacking toggle

--- Gobblepocalypse/Assets/Scripts/Player/PlayerScript.cs
+++ Gobblepocalypse/Assets/Scripts/Player/PlayerScript.cs
@@ -17,6 +17,9 @@ public class PlayerScript : MonoBehaviour
     public bool canShoot = true;
     public bool isCharging = false;
     public bool chargeTimerActive = false;
+    public bool isAttacking = false;
+    public bool attackTimerActive = false;
+    public bool attackCooldownTimerActive = false;
 
     //Vector Inputs
     public Vector3 mousePos;
@@ -25,18 +28,28 @@ public class PlayerScript : MonoBehaviour
     //Player Refs
     private Rigidbody2D rb;
 
+    //Line
+    public LineRenderer lr;
+
+    //Attacking
+    public float attackDuration;
+    public float attackTimer;
+    public float attackCooldownDuration;
+    public float attackCooldownTimer;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();   //Reference the players rigidbody2D
+        lr = gameObject.GetComponent<LineRenderer>();   //Reference the player's line renderer
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        #region Shooting
         if (Input.GetKey(KeyCode.Mouse0))   //If can shoot is true and left mouse is held
         {
             if (charges > 0)
@@ -55,17 +68,24 @@ public class PlayerScript : MonoBehaviour
                 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);     //Detect the mouse position with reference to main camera
                 mouseDistance = mousePos - transform.position;      //Calculate the distance from the mouse to the game object
                 #endregion
+                //Set line renderer here
+                #region Calculate Line
+                lr.enabled = true;  //Enable Line Renderer
+                lr.SetPosition(0, transform.position);
+                lr.SetPosition(1, mousePos);
+                #endregion
             }
-
-            //Set line renderer here
         }
 
         if (canShoot == true && isCharging == false && Input.GetKeyUp(KeyCode.Mouse0)) //On left mouse release
         { 
+            lr.enabled = false; //Disable Line Renderer
             rb.AddForce(mouseDistance * shootPower, ForceMode2D.Impulse);  //add a force to player in direction of mouse
             charges--;  //use a charge
         }
+        #endregion
 
+        #region Charging
         if (Input.GetKey(KeyCode.Mouse1))    //On right mouse hold
         { 
             canShoot = false;
@@ -95,6 +115,47 @@ public class PlayerScript : MonoBehaviour
                 }
             }
         }
+        #endregion
+
+        #region Attacking
+        if (attackCooldownTimerActive == false && Input.GetKeyDown(KeyCode.Space) && isAttacking == false)   //If attack is not on cd and player presses space
+        {
+            //activate attack mode
+            isAttacking = true;
+            //Start attack timer
+            attackTimer = attackDuration;   //Set the timer to its duration
+            attackTimerActive = true;       //Begin the attack timer countdown
+        }
+
+        if (attackTimerActive == true && isAttacking == true)   //If the attack timer is on
+        {
+            if (attackTimer > 0)
+            { 
+                attackTimer -= Time.deltaTime;  //Decrease timer until it hits 0
+            }
+
+            if (attackTimer <= 0)
+            {
+                isAttacking = false; //turn off attack mode
+                attackTimerActive = false;  //Stop the countdown and activate the cooldown
+                attackCooldownTimerActive = true;
+            }
+        }
+
+        if (isAttacking == false && attackCooldownTimerActive == true)   //If the cooldown timer is on
+        {
+            if (attackCooldownTimer < attackCooldownDuration)
+            { 
+                attackCooldownTimer += Time.deltaTime;  //increase timer by time passed until duration is hit
+            }
+
+            if (attackCooldownTimer >= attackCooldownDuration)
+            {
+                attackCooldownTimerActive = false;  //Turn off the cooldown boolean
+                attackCooldownTimer = 0;    //Reset timer
+            }
+        }
+        #endregion
     }
 
 }
