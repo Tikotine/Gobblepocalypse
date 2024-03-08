21eadaa206ca1c4e524d3752a077a9f251af80ca
Tikotine <77183360+Tikotine@users.noreply.github.com>
2024-03-08 13:40:47 +0800

    Boss resets on spikes and death zone

--- Gobblepocalypse/Assets/Scripts/Map Related/DeadZone.cs
+++ Gobblepocalypse/Assets/Scripts/Map Related/DeadZone.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DeadZone : MonoBehaviour
 {
     private CheckpointManager cm;
+    private Boss bossScript;
 
     public void Awake()
     {
         cm = GameObject.FindWithTag("CheckpointManager").GetComponent<CheckpointManager>();     //Find Checkpoint Manager
+        bossScript = GameObject.FindWithTag("Boss").GetComponent<Boss>();     //Find Boss script
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,6 +22,7 @@ public class DeadZone : MonoBehaviour
             cm.BossCheckpointReset();   //Reset boss to last checkpoint
             cm.player.GetComponent<PlayerScript>().ResetTimers();   //Reset attackmode timers
             cm.player.GetComponent<PlayerScript>().ResetVelocity(); //Reset player velocity
+            bossScript.SetCurrentState(new BossChase(bossScript));      //Reset boss state
         }
     }
 }
