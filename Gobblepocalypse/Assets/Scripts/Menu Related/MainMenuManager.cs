915c19006794e05bb00d5fa95ec73d939c08d432
Tikotine <timothykok@live.com>
2024-03-12 10:55:30 +0800

    Main Menu tutorial temp push

--- Gobblepocalypse/Assets/Scripts/Menu Related/MainMenuManager.cs
+++ Gobblepocalypse/Assets/Scripts/Menu Related/MainMenuManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("References")]
+    private GameObject mainCamera;
+    private Camera cam;
+
+    [Header("Tutorial Stuff")]
+    public Color[] backgroundColours;
+    public GameObject rightClick;
+    public GameObject leftClick;
+    public bool hasRightClicked;
+    public bool hasLeftClicked;
+
     //Button Prefabs
     [Header("Menu Button Prefabs")]
     public GameObject mainMenuButtonsPrefab;
@@ -20,11 +31,22 @@ public class MainMenuManager : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         playerScript = player.GetComponent<PlayerScript>();
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        cam = mainCamera.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!hasRightClicked && Input.GetKeyDown(KeyCode.Mouse1))
+        { 
+            FinishRightClickTutorial();
+        }
+
+        if (!hasLeftClicked && hasRightClicked && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FinishLeftClickTutorial();
+        }
     }
 
     public void SpawnMainMenu()
@@ -66,5 +88,20 @@ public class MainMenuManager : MonoBehaviour
     public void PlayButtonSound()
     {
         AudioManager.instance.PlayOneShot(FMODEvents.instance.buttonPress, transform.position);   //Play sound at button location
+    }
+
+    public void FinishRightClickTutorial()
+    {
+        hasRightClicked = true;
+        rightClick.SetActive(false);
+        leftClick.SetActive(true);
+    }
+
+    public void FinishLeftClickTutorial()
+    {
+        hasLeftClicked = true;
+        leftClick.SetActive(false);
+        cam.backgroundColor = backgroundColours[1];
+        SpawnMainMenu();
     }
 }
